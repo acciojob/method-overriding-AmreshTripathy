4579f30c25651be563dd117fade7d52d52cc4644
AmreshTripathy <amreshtripathy184@gmail.com>
2023-05-12 13:04:36 +0530

    method overriding push-4

---
+++ src/main/java/com/driver/Main.java
@@ -0,0 +1,27 @@
+package com.driver;
+
+public class Main {
+    static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    static class B extends A {
+
+        B() {
+            A obj = new A();
+            System.out.println(obj.meth());
+        }
+
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
+}
